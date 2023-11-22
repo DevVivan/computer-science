edf1532ead0d332f7ff6ca1dd7fd753bb43b1153
Vivan <vivanpanda13@gmail.com>
2023-11-22 18:27:43 +0400

    Complete task 2

--- 15-markers/parcelProgram.py
+++ 15-markers/parcelProgram.py
@@ -20,44 +20,62 @@ def calculateParcel(w, h, l, b):
         accepted = True
     else: 
         if (w > 10):
+            print("-" * 100)
             print("The parcel's weight exceeds " + str(maxWeight) + " kg.")
             accepted = False
         if (w < 1):
+            print("-" * 100)
             print("The parcel's weight is less than " + str(minWeight) + " kg.")
             accepted = False
         if (h + l + b > sumDimensions):
+            print("-" * 100)
             print("The sum of the dimensions of the parcel exceeds " + str(sumDimensions) + " cm.")
             accepted = False
         if (h > maxDimension):
+            print("-" * 100)
             print("The height of the parcel exceeds " + str(maxDimension) + " cm.")
             accepted = False
         if (l > maxDimension):
+            print("-" * 100)
             print("The length of the parcel exceeds " + str(maxDimension) + " cm.")
             accepted = False
         if (b > maxDimension):
+            print("-" * 100)
             print("The breadth of the parcel exceeds " + str(maxDimension) + " cm.")
             accepted = False
 
 def output():
-    global totalParcels, acceptedParcels, rejectedParcels, count
+    global totalParcels, acceptedParcels, rejectedParcels, count, acceptedWeight
     count = 0
     totalParcels = 0
     acceptedParcels = 0
     rejectedParcels = 0
+    acceptedWeight = 0
+    print("-" * 100)
     totalParcels = int(input("Enter the number of parcels you want to add to the consignment: "))
+    print("-" * 100)
     while count < totalParcels:
         getWeight()
         getDimensions()
         calculateParcel(weight, height, length, breadth)
         if accepted == False:
-            print("The parcel has not been accepted.")
-            rejectedParcels = rejectedParcels + 1
-            totalParcels = totalParcels + 1
+            print("-" * 100)
+            print("The parcel has not been accepted. Please try again.")
+            print("-" * 100)
+            getWeight()
+            getDimensions()
+            calculateParcel(weight, height, length, breadth)
+            print("-" * 100)
+            rejectedParcels += 1
         else:
+            print("-" * 100)
             print("The parcel has been accepted.")
-            acceptedParcels = acceptedParcels + 1
-            totalParcels = totalParcels + 1
+            print("-" * 100)
+            acceptedWeight = acceptedWeight + weight
+            acceptedParcels += 1
         count = count + 1
-        print(str(totalParcels, str(rejectedParcels), str(acceptedParcels)))
-
+    totalParcels = acceptedParcels + rejectedParcels
+    print("There are " + str(totalParcels) + " total parcels in your consignment.")
+    print(str(acceptedParcels) + " parcels were accepted. The total weight of these accepted parcels is " + str(acceptedWeight) + " kg." )
+    print(str(rejectedParcels) + " parcels were rejected.")
 output()
