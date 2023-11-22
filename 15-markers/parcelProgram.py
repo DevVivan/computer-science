50da71ad619ffc1e171071bbfdb13456845f507b
Vivan Panda <vivanpanda13@gmail.com>
2023-11-22 13:59:36 +0400

    Work on step 2 for parcelprogram

--- 15-markers/parcelProgram.py
+++ 15-markers/parcelProgram.py
@@ -39,12 +39,25 @@ def calculateParcel(w, h, l, b):
             accepted = False
 
 def output():
-    getWeight()
-    getDimensions()
-    calculateParcel(weight, height, length, breadth)
-    if accepted == False:
-        print("The parcel has not been accepted.")
-    else:
-        print("The parcel has been accepted.")
+    global totalParcels, acceptedParcels, rejectedParcels, count
+    count = 0
+    totalParcels = 0
+    acceptedParcels = 0
+    rejectedParcels = 0
+    totalParcels = int(input("Enter the number of parcels you want to add to the consignment: "))
+    while count < totalParcels:
+        getWeight()
+        getDimensions()
+        calculateParcel(weight, height, length, breadth)
+        if accepted == False:
+            print("The parcel has not been accepted.")
+            rejectedParcels = rejectedParcels + 1
+            totalParcels = totalParcels + 1
+        else:
+            print("The parcel has been accepted.")
+            acceptedParcels = acceptedParcels + 1
+            totalParcels = totalParcels + 1
+        count = count + 1
+        print(str(totalParcels, str(rejectedParcels), str(acceptedParcels)))
 
 output()
