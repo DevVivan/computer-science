22c08f1c14a54f175f743f4c3e6f31c10bba5efd
Vivan <vivanpanda13@gmail.com>
2023-11-27 16:51:30 +0400

    Work on learning function for this 15 marker

--- 15-markers/timesTables.py
+++ 15-markers/timesTables.py
@@ -15,14 +15,14 @@ def getName():
 def testing(x): 
     global score, count, times, userAnswer 
     while count <= 5: 
-		times = random.randint(1, 12) 
-		print("Question " + str(count)) 
-		userAnswer = input(str(x) + " x " + str(times) + " =") 
-		if  userAnswer == x * times: 
+        times = random.randint(1, 12) 
+        print("Question " + str(count)) 
+        userAnswer = int(input(str(x) + " x " + str(times) + " = "))
+        if userAnswer == x * times: 
             score = score + 1			 
         count = count + 1 
-    print(str(name) + " your score is " + str(score))
-	if score == 5: 
+    print(str(name) + ", your score is " + str(score))
+    if score == 5: 
         print("Well done, you got full marks.") 
     elif score == 4: 
         print("Good, you almost got full marks.") 
@@ -32,21 +32,46 @@ def testing(x):
         print("You didn't do very well. Practice is needed.")	 
 
 def learning(y): 
-	print("hi") 
+    global count
+    while count <= 5: 
+        times = random.randint(1, 12) 
+        print("Question " + str(count)) 
+        userAnswer = int(input(str(y) + " x " + str(times) + " = "))
+        if userAnswer == y * times: 
+            print(str(name) + ", your answer is correct!")		
+        if userAnswer != y * times:
+            if userAnswer > y * times:
+                print("This is incorrect, " + str(name) + ". Your answer is too large.") 
+            elif userAnswer > y * times:
+                print("This is incorrect, " + str(name) + ". Your answer is too small.") 
+            else:
+                print("This is not a valid answer. Try again.")
+    count = count + 1 
 
-def getMode(): 
-	global mode, table 
-	while True: 
-		print("[1] Testing") 
-		print("[2] Learning") 
+def runProgram(): 
+    global mode, table 
+    getName()
+    while True: 
+        print("*" * 100)
+        print("[1] Testing") 
+        print("[2] Learning") 
         print("[3] Exit")
-        mode = int(input("Enter your mode (1 for testing, 2 for learning): ")) 
-        table = int(input("Enter the times table you want to practice (Between 2 & 12)")) 
+        print("*" * 100)
+        mode = int(input("Enter your mode (1 for testing, 2 for learning, 3 to exit): ")) 
+        table = int(input("Enter the times table you want to practice (Between 2 & 12): ")) 
         if mode == 1: 
-            testing(table) 
+            if table < 2 or table > 12:
+                print("Invalid times table. It needs to be between 2 & 12. Try again.")
+            else:
+                testing(table) 
         elif mode == 2: 
-            Learning(table) 
+            if table < 2 and table > 12:
+                print("Invalid times table. It needs to be between 2 & 12. Try again.")
+            else:
+                learning(table) 
         elif mode == 3:
             break
         else: 
-            Print("Not a valid mode. Please try again.") 
+            print("Not a valid mode. Please try again.") 
+        
+runProgram()
