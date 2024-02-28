a6fe488cd1d2686b0da9804db7a796967b532e4f
Vivan Panda <vivanpanda13@gmail.com>
2024-02-28 13:35:22 +0400

    edit

--- 15-markers/hospital.py
+++ 15-markers/hospital.py
@@ -1,1 +1,14 @@
-Patient = []
+Patient = ["Bob","Jill","Ben","Grace"]
+Readings = [[35.60,56.00],[31.50,55.00],[32.60,101.00],[54.00, 101.00]]
+newPatient = ""
+tempRate = 0.0
+pulseRate - 0
+index = 0
+length = len(Patient)
+
+def getValues():
+    global index
+    newPatient = input("Enter the name of patient " + str(len + 1))
+    Patient[len] = newPatient
+
+getValues()
