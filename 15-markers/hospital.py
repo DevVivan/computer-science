177bc60b75b342bb6c82105de8d70a4cb7eab398
Vivan Panda <vivanpanda13@gmail.com>
2024-03-04 12:20:17 +0400

    add 15 marke

--- 15-markers/hospital.py
+++ 15-markers/hospital.py
@@ -2,13 +2,19 @@ Patient = ["Bob","Jill","Ben","Grace"]
 Readings = [[35.60,56.00],[31.50,55.00],[32.60,101.00],[54.00, 101.00]]
 newPatient = ""
 tempRate = 0.0
-pulseRate - 0
-index = 0
+pulseRate = 0
+counter = 0
 length = len(Patient)
 
 def getValues():
-    global index
-    newPatient = input("Enter the name of patient " + str(len + 1))
-    Patient[len] = newPatient
+    global counter, hospitalNumber
+    while counter < length:
+        hospitalNumber = int(input("Enter the patient's hospital number: "))
+        if hospitalNumber < 1 or hospitalNumber > 1000:
+            print("The hospital number is invalid. It must be between 1 and 1000.")
+            break
+        else:
+            print("The patient is " + Patient[hospitalNumber - 1])
+        counter = counter + 1
 
 getValues()
