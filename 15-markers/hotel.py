c70ceb103a49b82a70b7bb480850254bc15088a5
Vivan Panda <vivanpanda13@gmail.com>
2024-05-20 11:52:44 +0400

    work on new

--- 15-markers/hotel.py
+++ 15-markers/hotel.py
@@ -7,17 +7,26 @@ nights = 0
 length = len(Bookings)
 row = 0
 length2 = len(Bookings[row])
+room = ""
+cost = 0.00
 
 while row < length:
     col = 0
     name = input("Enter guest " + str(row + 1) + "'s name: ")
     Guests[row] = name
     while col < length2:
-        col = col + 1
         nights = int(input("Enter the number of nights you will stay, " + Guests[row] + ": "))
         while nights < 1 or nights > 30:
             print("Incorrectly entered number of nights, it must be between 1-30. Please try again.")
             nights = int(input("Enter the number of nights you will stay, " + Guests[row] + ": "))
+        for i in range(3):
+            print(str(i) + ". " + RoomType[i])
+        room = int(input("Enter the type of room you would like to book: "))
+        cost = Price[room] * nights
+        print("The total cost is " + str(cost))
+        cost = 0.00
+        room = ""
+        nights = 0
+        name = ""
+        col = col + 1
     row = row + 1
-
-print(Guests)
